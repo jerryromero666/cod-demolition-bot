98d29fc74c12905fce8aceeaa8de27cbab42a57d
jerryromero666 <jerry.romero178@gmail.com>
2026-05-26 10:52:24 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ API_KEY_SECRET = os.getenv("API_KEY_SECRET")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
 
-TARGET_ACCOUNTS = ["CallofDuty", "Treyarch", "CallofDutyCM"]
+TARGET_ACCOUNTS = ["CallofDuty", "Treyarch", "CallofDutyCM", "MaTtKs," "Activision", "CODUpdates", "ATVI_AB"]
 REPLY_MESSAGE = "Bring DEMOLITION to HARDCORE in BO7 PLEASE!!!! We're still stuck playing Cold War to enjoy HC Demo - there's DOZENS OF US!!!! PLzzzZzzZ <3"
 
 last_seen_tweets = {account: None for account in TARGET_ACCOUNTS}
